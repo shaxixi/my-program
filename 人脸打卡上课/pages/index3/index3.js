84f768a69e6a506b504dc6fc2742a41de69b3630
shaxixi <71205206+shaxixi@users.noreply.github.com>
2021-02-21 19:46:40 +0800

    基本实现项目——前端

---
+++ 人脸打卡上课/pages/index3/index3.js
@@ -0,0 +1,21 @@
+// pages/index3/index3.js
+Page({
+  wode(){
+    wx.navigateTo({
+      url: '../index3/wo_de/wo_de',
+    })
+  },
+
+  guanyu(){
+    wx.navigateTo({
+      url: '../index3/guan_yu/guan_yu',
+    })
+  },
+
+  shuju(){
+    wx.navigateTo({
+      url: '../index3/shu_ju/shu_ju',
+    })
+  },
+ 
+})
